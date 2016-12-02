7a6e35e6320820c86d51998ecb637665289b3406
Francisco Montiel <fjmontiel@spiralapps.com>
2016-12-02 16:45:33 +0100

    Push

---
+++ pushsample/src/main/java/com/franmontiel/pushsample/MyFirebaseMessagingService.java
@@ -0,0 +1,50 @@
+package com.franmontiel.pushsample;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Francisco J. Montiel on 2/12/16.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d("FCM", "Message Received");
+
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+        stackBuilder.addNextIntent(new Intent(getApplicationContext(), MainActivity.class))
+                .addNextIntent(new Intent(getApplicationContext(), NotificationOpenedActivity.class));
+
+        PendingIntent contentIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent))
+//                .setTicker(getString(R.string.app_name))
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Contenido de la notificación")
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .setContentIntent(contentIntent)
+                .setDefaults(Notification.DEFAULT_SOUND);
+
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(null, 1, notificationBuilder.build());
+    }
+}
